79c3a52b89a58f89e9637ef8f4db79450b2835e6
Danielle de Sotti <danidesotti@Air-de-Danielle.Home>
2022-11-28 23:52:02 +0000

    28.11.2022

---
+++ ft_printf.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dde-sott <dde-sott@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 23:07:36 by dde-sott          #+#    #+#             */
+/*   Updated: 2022/11/28 23:51:18 by dde-sott         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//Na função principal vericar qual o tipo de variavel recebi e chamar cada uma das funções;
+//utilizar os parametros variadic que dá a capacidade de imprimir varias variaveis e mais do que 1 tipo;
+//Criar varias funções para imprimir números, string, numeros hexadecimais etc etc;
+//Encontrar uma forma de ler %d, %i por exemplo
+//1. Descobrir quantos parametros a função tem;
+#include <stdarg.h>
+
+int ft_printf(const char *, ...)
+{
+    //va_list args;
+    //va_start;
+    //va_end.
+    
+    /*%c Prints a single character.
+    %s Prints a string (as defined by the common C convention).
+    %p The void * pointer argument has to be printed in hexadecimal format.
+    %d Prints a decimal (base 10) number.
+    %i Prints an integer in base 10.
+    %u Prints an unsigned decimal (base 10) number.
+    %x Prints a number in hexadecimal (base 16) lowercase format.
+    %X Prints a number in hexadecimal (base 16) uppercase format.
+    %% Prints a percent sign.*/
+
+}
