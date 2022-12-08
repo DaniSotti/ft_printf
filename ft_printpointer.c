7c8c3401050585a21f54dd2db9db5325d9b7761a
Danielle de Sotti <danidesotti@Air-de-Danielle.Home>
2022-12-08 15:41:45 +0000

    08.12.2022

--- ft_printpointer.c
+++ ft_printpointer.c
@@ -12,29 +12,37 @@ static int	ft_hexalen(unsigned int decimal)
 	}
 	return (len);
 }
-int	ft_printpointer(unsigned long long decimal)
+int	ft_printpointer(unsigned long long pointer)
 {
 	char hexa[25];
 	int	len;
     int i = 0;
     int remain;
 
-	len = ft_hexalen(decimal);
-    write(1, "0x", 2);
-	if (decimal == 0)
-		return (ft_putchar('0'));
-    while (decimal != 0)
+	//len = ft_hexalen(decimal);
+    len = 0;
+    len += write(1, "0x", 2);
+	if (pointer == 0)
     {
-        remain = decimal % 16;
-        if (remain < 10)
-            remain = 48 + remain;
-        else
-            remain = remain + 87;
-        hexa[i] = remain;
-        decimal = decimal / 16;
-        i++;
+		len += write(1, "0", 5);
+        return (len);
     }
-    while (i--)
-		ft_putchar(hexa[i]);
-	return (len);
+    else
+    {
+        while (pointer != 0)
+        {
+            len += ft_hexalen(pointer);
+            remain = pointer % 16;
+            if (remain < 10)
+                remain = 48 + remain;
+            else
+                remain = remain + 87;
+            hexa[i] = remain;
+            pointer = pointer / 16;
+            i++;
+        }
+        while (i--)
+            ft_putchar(hexa[i]);
+        return (len);
+    }
 }
